f5c57474c50ec7d161c0842d576adb330c77c68d
Ignacio <dignaciocassi@gmail.com>
2023-12-22 16:47:42 -0300

    Solved '2-Cube Conudrum-Part 2'

--- src/main/java/org/example/CubeConudrum.java
+++ src/main/java/org/example/CubeConudrum.java
@@ -6,22 +6,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CubeConudrum {
 
-    public int sumPossibleGames(Map<String, Integer> game) {
-        String gameFile = readFile("cubes.txt");
+    public int sumPossibleGames(Map<String, Integer> game, String filename) {
+        String gameFile = readFile(filename);
         int sum = 0;
         String[] lines = gameFile.split("\n");
         for (String line: lines) {
             String[] lineFields = line.split(": |:|; |, ");
-            int gameId = Integer.valueOf(lineFields[0].split(" ")[1]);
+            int gameId = Integer.parseInt(lineFields[0].split(" ")[1]);
             int i = 1;
             boolean possible = true;
             while (i < lineFields.length && possible) {
                 String[] seen = lineFields[i].split(" ");
-                int quantity = Integer.valueOf(seen[0]);
+                int quantity = Integer.parseInt(seen[0]);
                 String color = seen[1];
                 if (quantity > game.get(color)) {
                     possible = false;
@@ -31,6 +32,32 @@ public class CubeConudrum {
             if (possible) {
                 sum += gameId;
             }
+        }
+        return sum;
+    }
+
+    public int sumPowerFewestNeeded(String filename) {
+        // find fewest needed of each colour for each game
+        // multiply fewestRed * fewestGreen * fewestBlue for each game
+        // return sum every game power
+        String gameFile = readFile(filename);
+        int sum = 0;
+        Map<String, Integer> fewestNeeded = new HashMap<>();
+        String[] lines = gameFile.split("\n");
+        for (String line: lines) {
+            fewestNeeded.put("red", 0);
+            fewestNeeded.put("green", 0);
+            fewestNeeded.put("blue", 0);
+            String[] gameFields = line.split(": |:|; |, ");
+            for (int i = 1; i < gameFields.length; i++) {
+                String[] data = gameFields[i].split(" ");
+                int quantity = Integer.parseInt(data[0]);
+                String colour = data[1];
+                if (quantity > fewestNeeded.get(colour)) {
+                    fewestNeeded.put(colour, quantity);
+                }
+            }
+            sum += fewestNeeded.get("red") * fewestNeeded.get("green") * fewestNeeded.get("blue");
         }
         return sum;
     }
--- src/test/java/org/example/CubeConudrumTest.java
+++ src/test/java/org/example/CubeConudrumTest.java
@@ -1,22 +1,33 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CubeConudrumTest {
 
-    @Test
-    void sumPossibleGames() {
-        Map<String, Integer> games = new HashMap<>();
+    private static Map<String, Integer> games;
+
+    private static CubeConudrum cubeConudrum;
+
+    @BeforeAll
+    static void beforeAll() {
+        cubeConudrum = new CubeConudrum();
+        games = new HashMap<>();
         games.put("red", 12);
         games.put("green", 13);
         games.put("blue", 14);
-        CubeConudrum cubeConudrum = new CubeConudrum();
-        Assertions.assertEquals(2528, cubeConudrum.sumPossibleGames(games));
+    }
+
+    @Test
+    void sumPossibleGames() {
+        Assertions.assertEquals(2528, cubeConudrum.sumPossibleGames(games, "cubes.txt"));
+    }
+
+    @Test
+    void sumPowerFewestNeeded() {
+        Assertions.assertEquals(67363, cubeConudrum.sumPowerFewestNeeded("cubes.txt"));
     }
 }
